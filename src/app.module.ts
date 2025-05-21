ec505cf6e24cd0ea93c3999e67666696d9ba121d
Azizbek2204-B <xashimudfarrux@example.com>
2025-05-21 18:32:06 +0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
+import { TeacherModule } from './teacher/teacher.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type:"mysql",
+      host:"localhost",
+      port:3306,
+      username:"root",
+      password:"22",
+      database:"crm",
+      entities:[],
+      synchronize:true
+    }),
+    AdminModule,
+    AuthModule,
+    TeacherModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
